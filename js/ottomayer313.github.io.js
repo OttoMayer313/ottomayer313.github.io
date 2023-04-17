7ec1e9515d45f861ae2b6e84059bc72bace1379c
ottomayer313 <fiverscan.ks3p@gmail.com>
2023-04-17 11:13:24 -0700

    profile update

--- js/ottomayer313.github.io.js
+++ js/ottomayer313.github.io.js
@@ -28,7 +28,7 @@ Licensed under GNU GPL (https://www.gnu.org/licenses/licenses.html).
 
     var text;
     var outputedToConsole = false;
-    var commands = ["whoami?", "finger otto", "cat " + ABOUTME];
+    var commands = ["Who am I?", "I'm a passionate full stack developer with a full knowledge in frontend & backend development but also blockchain and cloud computing.", "cat " + ABOUTME];
 
     // var keySound1;
 
